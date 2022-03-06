cda78420ea686073225551125d74b4c0cad2a21f
ugo <ugovaretto@gmail.com>
2022-03-06 17:39:57 +0800

    Created

---
+++ init.lua
@@ -0,0 +1,169 @@
+local o = vim.o
+local bo = vim.bo
+wo = vim.wo
+o.termguicolors = true
+o.syntax = 'on'
+o.errorbells = false
+o.smartcase = true
+o.showmode = false
+bo.swapfile = false
+o.backup = false
+o.undodir = vim.fn.stdpath('config') .. '/undodir'
+o.undofile = true
+o.incsearch = true
+o.hidden = true
+o.completeopt='menuone,noinsert,noselect'
+bo.autoindent = true
+bo.smartindent = true
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+wo.number = true
+wo.relativenumber = true
+wo.signcolumn = 'yes'
+wo.wrap = false
+o.colorcolumn = '80'
+vim.cmd[[highlight ColorColumn ctermbg=0 guibg=grey]]
+vim.g.mapleader = ' '
+
+------------------------------------------------------------------------------
+-- Key mapping
+local key_mapper = function(mode, key, result)
+  vim.api.nvim_set_keymap(
+    mode,
+    key,
+    result,
+    {noremap = true, silent = true}
+  )
+end
+
+------------------------------------------------------------------------------
+-- Packer
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
+  -- add you plugins here like:
+  -- use 'neovim/nvim-lspconfig'
+  end
+)
+
+------------------------------------------------------------------------------
+packer.startup(function()
+  local use = use
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'sheerun/vim-polyglot'
+  -- these are optional themes but I hear good things about gloombuddy ;)
+  -- colorbuddy allows us to run the gloombuddy theme
+  use 'tjdevries/colorbuddy.nvim'
+  use 'bkegley/gloombuddy'
+  -- sneaking some formatting in here too
+  use {'prettier/vim-prettier', run = 'yarn install' }
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/completion-nvim'
+  use 'anott03/nvim-lspinstall'
+  -- Rust
+  use "rust-lang/rust.vim"
+  use "rhysd/rust-doc.vim"
+  use {"neoclide/coc.nvim", branch = 'release'}
+  use "simrat39/rust-tools.nvim"
+  -- Fuzzy finding
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-lua/telescope.nvim'
+  use 'jremmen/vim-ripgrep'
+  -- Git 
+  use "lewis6991/gitsigns.nvim"
+  -- Status bar
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  } 
+  use "kyazdani42/nvim-web-devicons"
+  -- Color schemes
+  use 'tomasiser/vim-code-dark'
+  end
+)
+
+-- Treesitter
+vim.g.colors_name = 'gloombuddy'
+
+local configs = require'nvim-treesitter.configs'
+configs.setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true,
+  }
+}
+
+-- LSP
+local lspconfig = require'lspconfig'
+local completion = require'completion'
+local function custom_on_attach(client)
+  print('Attaching to ' .. client.name)
+  completion.on_attach(client)
+end
+local default_config = {
+  on_attach = custom_on_attach,
+}
+-- setup language servers here
+--lspconfig.tsserver.setup(default_config)
+    
+require('lspconfig')
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'rust_analyzer' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+  on_attach = on_attach,
+  flags = {
+    -- This will be the default in neovim 0.7+
+    debounce_text_changes = 150,
+  }
+}
+end
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+  }
+)
+key_mapper('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
+key_mapper('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
+key_mapper('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
+key_mapper('n', 'gw', ':lua vim.lsp.buf.document_symbol()<CR>')
+key_mapper('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>')
+key_mapper('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
+key_mapper('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
+key_mapper('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
+key_mapper('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>')
+key_mapper('n', '<leader>af', ':lua vim.lsp.buf.code_action()<CR>')
+key_mapper('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
+-- Fuzzy finding
+key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
+key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
+key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
+key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
+-- Lualine
+require('lualine').setup()
+
+vim.cmd[[colorscheme codedark]]
+--vim.cmd('source  $HOME/.config/nvim/coc-settings.vim')
